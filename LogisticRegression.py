839c1d5cf94a9b800b475cc3da7f859ee34a540b
Ranya <20bluebell04@gmail.com>
2025-10-11 12:06:59 +0100

    adding C hyperparameter tuning

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
@@ -35,4 +36,46 @@ plt.xlabel('Actual', fontsize=13)
 plt.title('Default LR - Fire Prediction vs Actual', fontsize=17)
 plt.show()
 
+# Tuning hyperparameter 1: C
+c_values = np.arange(1,50)
 
+training_accuracy = []
+test_accuracy = []
+for i in c_values:
+    lr = LogisticRegression(max_iter=5000, C=i)
+
+    lr.fit(x_train, y_train)
+    prediction = lr.predict(x_test)
+    training_accuracy.append(lr.score(x_train, y_train))
+    test_accuracy.append(lr.score(x_test, y_test))
+
+plt.plot(c_values,training_accuracy, marker = 'o', color = 'b',label='training accuracy')
+plt.plot(c_values,test_accuracy, marker = 'o', color = 'r',label='testing accuracy')
+plt.xlabel('c_parameter')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+diff = np.array(training_accuracy) - np.array(test_accuracy)
+min_idx = np.argmin(diff)
+
+lr = LogisticRegression(max_iter=1000, C=c_values[min_idx])
+lr.fit(x_train, y_train)
+
+prediction_c_tuning = lr.predict(x_test)
+print("C Tuning - Logistic Regression Accuracy:", metrics.accuracy_score(y_test, prediction_c_tuning))
+
+cm_c_tuning = confusion_matrix(prediction_c_tuning, y_test)
+
+sns.heatmap(cm_c_tuning,
+            annot=True,
+            fmt='g',
+            cmap='Blues',
+            xticklabels=['Yes', 'No'],
+            yticklabels=['Yes', 'No'],
+            square=True,
+            cbar=False)
+plt.ylabel('Prediction', fontsize=13)
+plt.xlabel('Actual', fontsize=13)
+plt.title('C Tuning LR - Fire Prediction vs Actual', fontsize=17)
+plt.show()
