ea2a29ed37a468bc5ec598a8c154c51a78278963
Ranya <20bluebell04@gmail.com>
2025-10-16 15:42:54 +0100

    adding back in training accuracy

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -8,6 +8,7 @@ import seaborn as sns
 from sklearn.exceptions import ConvergenceWarning
 import warnings
 
+#  data sets
 training_data = pd.read_csv('wildfires_training.csv')
 test_data = pd.read_csv('wildfires_test.csv')
 
@@ -16,83 +17,36 @@ y_train = training_data['fire']
 x_test = test_data.drop("fire", axis=1)
 y_test = test_data['fire']
 
-# finding best max_iter for convergence
-max_iter = 100
-for max_iter in range(100, 1000, 10):
-    clf = LogisticRegression(max_iter=max_iter)
-    with warnings.catch_warnings(record=True) as w:
-        warnings.simplefilter("always", ConvergenceWarning)
-        clf.fit(x_train, y_train)
-        if not any(isinstance(wi.message, ConvergenceWarning) for wi in w):
-            print(f"Converged with max_iter={max_iter}")
-            break
-
-
-# Default
+# Default logistic regression
 lr = LogisticRegression(max_iter=370)
 lr.fit(x_train, y_train)
 
 prediction_default = lr.predict(x_test)
 print("Default Logistic Regression Accuracy:", metrics.accuracy_score(y_test, prediction_default))
 
-cm = confusion_matrix(prediction_default, y_test)
-
-sns.heatmap(cm,
-            annot=True,
-            fmt='g',
-            cmap='Blues',
-            xticklabels=['Yes', 'No'],
-            yticklabels=['Yes', 'No'],
-            square=True,
-            cbar=False)
-plt.ylabel('Prediction', fontsize=13)
-plt.xlabel('Actual', fontsize=13)
-plt.title('Default LR - Fire Prediction vs Actual', fontsize=17)
-plt.show()
-
 # Tuning hyperparameter 1: C
 c_values = np.arange(1,50)
 
-training_accuracy = []
 test_accuracy = []
+training_accuracy = []
+
 for i in c_values:
-    lr = LogisticRegression(max_iter=5000, C=i)
+    lr = LogisticRegression(max_iter=1000, C=i)
 
     lr.fit(x_train, y_train)
-    prediction = lr.predict(x_test)
-    training_accuracy.append(lr.score(x_train, y_train))
-    test_accuracy.append(lr.score(x_test, y_test))
+    prediction_test = lr.predict(x_test)
+    prediction_training = lr.predict(x_train)
+    test_accuracy.append(metrics.accuracy_score(y_test, prediction_test))
+    training_accuracy.append(metrics.accuracy_score(y_train, prediction_training))
 
-plt.plot(c_values,training_accuracy, marker = 'o', color = 'b',label='training accuracy')
 plt.plot(c_values,test_accuracy, marker = 'o', color = 'r',label='testing accuracy')
+plt.plot(c_values,training_accuracy, marker = 'o', color = 'b',label='training accuracy')
 plt.xlabel('c_parameter')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
 
-diff = np.array(training_accuracy) - np.array(test_accuracy)
-min_idx = np.argmin(diff)
-
-lr = LogisticRegression(max_iter=370, C=c_values[min_idx])
-lr.fit(x_train, y_train)
-
-prediction_c_tuning = lr.predict(x_test)
-print("C Tuning - Logistic Regression Accuracy:", metrics.accuracy_score(y_test, prediction_c_tuning))
-
-cm_c_tuning = confusion_matrix(prediction_c_tuning, y_test)
-
-sns.heatmap(cm_c_tuning,
-            annot=True,
-            fmt='g',
-            cmap='Blues',
-            xticklabels=['Yes', 'No'],
-            yticklabels=['Yes', 'No'],
-            square=True,
-            cbar=False)
-plt.ylabel('Prediction', fontsize=13)
-plt.xlabel('Actual', fontsize=13)
-plt.title('C Tuning LR - Fire Prediction vs Actual', fontsize=17)
-plt.show()
+print("The best accuracy of",np.max(test_accuracy),"was found at C = ", c_values[np.argmax(test_accuracy)])
 
 # Tuning hyperparameter 2: Solver + Penalty
 
@@ -155,3 +109,13 @@ plt.title('Accuracy for Each Penalty and Solver Combination')
 plt.tight_layout()
 plt.show()
 
+# finding best max_iter for convergence
+max_iter = 100
+for max_iter in range(100, 1000, 10):
+    clf = LogisticRegression(max_iter=max_iter)
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always", ConvergenceWarning)
+        clf.fit(x_train, y_train)
+        if not any(isinstance(wi.message, ConvergenceWarning) for wi in w):
+            print(f"Converged with max_iter={max_iter}")
+            break
--- SupportVectorMachine.py
+++ SupportVectorMachine.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn import metrics
 from sklearn import svm
-from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
 # data sets
@@ -25,18 +24,25 @@ print("Default SVM Accuracy:", metrics.accuracy_score(y_test, prediction_svm))
 kernels = ['linear', 'poly', 'rbf', 'sigmoid']
 
 test_accuracy = []
+training_accuracy = []
 
 for kernel in kernels:
     clf = svm.SVC(kernel=kernel)
 
     clf.fit(x_train, y_train)
-    prediction = clf.predict(x_test)
-    test_accuracy.append(metrics.accuracy_score(y_test, prediction))
+    prediction_test = clf.predict(x_test)
+    prediction_training = clf.predict(x_train)
+    test_accuracy.append(metrics.accuracy_score(y_test, prediction_test))
+    training_accuracy.append(metrics.accuracy_score(y_train, prediction_training))
 
 best_kernel = kernels[np.argmax(test_accuracy)]
-print("The", best_kernel, "Kernel gives the best accuracy of", np.max(test_accuracy))
+print("The", best_kernel, "Kernel gives the best accuracy of", np.max(test_accuracy), "in the test set.")
+
+best_kernel = kernels[np.argmax(training_accuracy)]
+print("The", best_kernel, "Kernel gives the best accuracy of", np.max(training_accuracy), "in the training set.")
 
 plt.plot(kernels, test_accuracy, marker = 'o', color = 'r',label='testing accuracy')
+plt.plot(kernels, training_accuracy, marker = 'o', color = 'b',label='training accuracy')
 plt.xlabel('Kernels')
 plt.ylabel('Accuracy')
 plt.legend()
@@ -44,36 +50,48 @@ plt.show()
 
 
 # Tuning hyperparameter 2: C
+# Using default kernel
 c_values = np.arange(1,100)
 
 test_accuracy = []
+training_accuracy = []
+
 for i in c_values:
     clf = svm.SVC(C=i)
 
     clf.fit(x_train, y_train)
-    prediction = clf.predict(x_test)
-    test_accuracy.append(metrics.accuracy_score(y_test, prediction))
+    prediction_test = clf.predict(x_test)
+    prediction_training = clf.predict(x_train)
+    test_accuracy.append(metrics.accuracy_score(y_test, prediction_test))
+    training_accuracy.append(metrics.accuracy_score(y_train, prediction_training))
 
 plt.plot(c_values,test_accuracy, marker = 'o', color = 'r',label='testing accuracy')
+plt.plot(c_values,training_accuracy, marker = 'o', color = 'b',label='training accuracy')
 plt.xlabel('c_parameter')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
 
 print("The highest accuracy of the default Kernel is", np.max(test_accuracy), "when C is", c_values[np.argmax(test_accuracy)])
+print("The highest accuracy of the default Kernel is", np.max(training_accuracy), "when C is", c_values[np.argmax(training_accuracy)])
 
-
+# Using the best Kernel - linear
 c_values = np.arange(1,100)
 
 test_accuracy = []
+training_accuracy = []
 
 for i in c_values:
     clf = svm.SVC(kernel='linear', C=i)
     clf.fit(x_train, y_train)
-    prediction = clf.predict(x_test)
-    test_accuracy.append(metrics.accuracy_score(y_test, prediction))
+    prediction_training = clf.predict(x_train)
+    prediction_test = clf.predict(x_test)
+
+    test_accuracy.append(metrics.accuracy_score(y_test, prediction_test))
+    training_accuracy.append(metrics.accuracy_score(y_train, prediction_training))
 
 plt.plot(c_values,test_accuracy, marker = 'o', color = 'r',label='testing accuracy')
+plt.plot(c_values,training_accuracy, marker = 'o', color = 'b',label='training accuracy')
 plt.xlabel('c_parameter')
 plt.ylabel('Accuracy')
 plt.legend()
@@ -82,4 +100,8 @@ plt.show()
 max_test_accuracy = np.max(test_accuracy)
 max_test_idx = np.argmax(max_test_accuracy)
 
-print("The highest accuracy of the linear kernel is", max_test_accuracy, "at C = ", max_test_idx)
+print("The highest accuracy of the linear kernel is", max_test_accuracy, "at C = ", max_test_idx, "in the test set")
+
+max_training_accuracy = np.max(training_accuracy)
+max_training_idx = np.argmax(max_training_accuracy)
+print("The highest accuracy of the linear kernel is", max_training_accuracy, "at C = ", max_training_idx, "in the test set")
